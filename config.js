0f5d2082872abee3a8a5d0d4da002ca0c1e7d968
MrBat501 <jorgexxgarci@gmail.com>
2026-08-30 23:32:12 -0600

    Update config.js

--- config.js
+++ config.js
@@ -4,5 +4,5 @@
 //   - "Project URL"      -> SUPABASE_URL
 //   - "anon public" key  -> SUPABASE_ANON_KEY
 
-const SUPABASE_URL = "PEGA_AQUI_TU_PROJECT_URL";
-const SUPABASE_ANON_KEY = "PEGA_AQUI_TU_ANON_KEY";
+const SUPABASE_URL = "https://zywnqpouolnyurndhvus.supabase.co";
+const SUPABASE_ANON_KEY = sb_publishable_PhxTmKC6SatUBQ5of_U4vg_VWfnyxOG;
